7592be599fe90edc6283ea6ffbd5749a548393c5
Elizabeth Larson <eml2017@yahoo.com>
2020-12-06 16:28:17 -0800

    Installed cocoapod for viewing youtube (unsucessful) + wrote up code and design for user video preview

--- Mood-Board-Designer/Layout1ViewController.swift
+++ Mood-Board-Designer/Layout1ViewController.swift
@@ -4,29 +4,38 @@
 //
 //  Created by Elizabeth Larson on 12/3/20.
 //
+//  Sources
+//  YouTube Player: https://www.youtube.com/watch?v=bsM1qdGAVbU
+//  Only Getting a Portion of the Video URL: https://stackoverflow.com/questions/34073610/swift-how-to-get-everything-after-a-certain-set-of-characters
 
+// MARK: - A Note on the Commented-Out Code
+//      CocoaPod was installed, but import line reads "No such module 'youtube_ios_player_helper'", despite it being in the Podfile.
+//      This messes up some other functionality. I tried this on another project and everything worked fine.
+//      These lines have a header above them that reads ------------- YouTube Functionality: Uncomment Below -------------
+
+// ------------- YouTube Functionality: Uncomment Below -------------
+// import youtube_ios_player_helper
 import UIKit
 
+// ------------- YouTube Functionality: Uncomment Below -------------
+// class Layout1ViewController: UIViewController, YTPlayerViewDelegate {
 class Layout1ViewController: UIViewController {
-    @IBOutlet var nameTextField: UITextField!
-    @IBAction func saveButton(_ sender: UIButton) {
+    // ------------- YouTube Functionality: Uncomment Below -------------
+    //@IBOutlet var videoPreview: YTPlayerView!
+    @IBOutlet var urlTextField: UITextField!
+    @IBAction func viewInPlayerButton(_ sender: UIButton) {
+        if let fullUrl = urlTextField.text {
+            if let range = fullUrl.range(of: "=") {
+                // videoID is the stuff after the = sign in a YouTube link
+                let videoID = fullUrl[range.upperBound...]
+                // ------------- YouTube Functionality: Uncomment (2) Below -------------
+                //videoPreview.delegate = self
+                //videoPreview.load(withVideoId: "" + videoID, playerVars: ["playsinline": 1])
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.2'
+
+target 'Mood-Board-Designer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Mood-Board-Designer
+  pod 'youtube-ios-player-helper'
+
+end
